4ef06986174927139d1df7fa9c489f044c62d12c
cheekyorkind <gkbn7y7h>
2017-10-30 21:56:53 +0900

    getsTest.sol의 삭제, getsTest1.sol로 이름을 바꾸고 테스트를 진행한다.

--- applicationLogics/getsTest1.sol
+++ applicationLogics/getsTest1.sol
@@ -1,6 +1,7 @@
-contract Simple {
 	// 어떤 형태의 function이 React Native에서 값을 받을수 있는지 테스트한다.
-
+	// debugger-ui : BigNumber {s: 1, e: 1, c: Array(1)} 형태로 return 받은것을 확인함.
+	// React Native : result.c[0] 형식으로 값을 받아올수 있음.
+contract Simple {
     uint storedData;
 
     function set(uint x) {
@@ -11,3 +12,4 @@ contract Simple {
         return storedData;
     }
 }
+
