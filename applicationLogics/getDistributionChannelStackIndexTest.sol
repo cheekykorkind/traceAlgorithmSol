ee06d7c64d351636ad301d00568ae2eb3029c507
cheekyorkind <gkbn7y7h>
2017-11-15 18:01:38 +0900

    Remix에서 정상동작

--- applicationLogics/getDistributionChannelStackIndexTest.sol
+++ applicationLogics/getDistributionChannelStackIndexTest.sol
@@ -14,37 +14,40 @@
 pragma solidity ^0.4.0;
 contract DistributionChannelTrace {
     struct currentTx {  //  현 시점에서 일어난 도매업자간의 거래를 의미하는 자료구조이다.
-        // string _txIndex;
+        string _txIndex;
         address _sellerAddr;
         address _buyerAddr;
-        // string _itemName;
-        // uint _volum;
+        string _itemName;
+        uint _volum;
         uint _totalPrice;
         string _date;
     }
     
-    struct getDistributionChannelStackInfo{
-        bool _doesHaveTx;
-        uint _mappingCounter;
-        uint _selectedMappingLength;
-    }
+    // struct getDistributionChannelStackInfo{
+    //     bool _doesHaveTx;
+    //     uint _mappingCounter;
+    //     uint _selectedMappingLength;
+    // }
 
     mapping(uint => currentTx[]) public distributionChannelTable;
 
-    function sendCurrentTx(address sellerAddr, address buyerAddr, uint totalPrice, string date) public {
+    function sendCurrentTx(string txIndex, address sellerAddr, address buyerAddr, string itemName, uint volum, uint totalPrice, string date) public {
         if(isTableEmpty()){ // 유통 경로 테이블이 비었다. stack을 만들면서 currentTx를 삽입한다.
-            pushInitialTx(sellerAddr, buyerAddr, totalPrice, date);
-        }else if(!pushTxOnPreviousTx(sellerAddr, buyerAddr, totalPrice, date)){  //  true는 이전거래 삽입이 끝난 상태이다.
-            pushTxForNewDistributionChannel(sellerAddr, buyerAddr, totalPrice, date);
+            pushInitialTx(txIndex, sellerAddr, buyerAddr, itemName, volum, totalPrice, date);
+        }else if(!pushTxOnPreviousTx(txIndex, sellerAddr, buyerAddr, itemName, volum, totalPrice, date)){  //  true는 이전거래 삽입이 끝난 상태이다.
+            pushTxForNewDistributionChannel(txIndex, sellerAddr, buyerAddr, itemName, volum, totalPrice, date);
         }
     }
 
     // 유통 경로 테이블이 비었다. stack을 만들면서 currentTx를 삽입한다.
     // function pushInitialTx(address sellerAddr, address buyerAddr, uint totalPrice, string date) public {
-    function pushInitialTx(address sellerAddr, address buyerAddr, uint totalPrice, string date) public {
+    function pushInitialTx(string txIndex, address sellerAddr, address buyerAddr, string itemName, uint volum, uint totalPrice, string date) public {
         distributionChannelTable[0].push(currentTx(
+            txIndex,
             sellerAddr,
             buyerAddr,
+            itemName,
+            volum,
             totalPrice,
             date
         ));
@@ -56,54 +59,38 @@ contract DistributionChannelTrace {
 		3. 두번쨰 uint는 mapping에서 스택의 위치를 알려주고, 세번째 uint는 stack의 반복횟수를 알려준다.
 		4. Android에서 getDistributionChannelStackIndex()를 getMappingLength()만큼 호출해서 이름이 distributionChannels 인자가 (r_stackIndex, r_stackLength)인 동적 배열을 만들고
 			stack 1개의 모든 값을 돌려주는 function을 distributionChannels의 인자수 만큼 호출해서 모든 유통 값을 받는다.
+		별도1. android device는 getMappingLength()횟수만큼 loop를 진행하는데, mappingCounter가 loopcounter가 들어간다.
+		즉, getMappingLength()만큼 getDistributionChannelStackIndex()를 호출한다.
+		별도2. true인 데이터셋을 모아서 getTx()를 호출한다.
 	*/
-    function getDistributionChannelStackIndex(address retailTraderAddress, string itemName)  public returns(bool, uint, uint){
-        getDistributionChannelStackInfo[] channelsInfo;
-        uint maxMappingLength = getMappingLength();
-        
-        uint mappingCounter=0;  //  mapping 에서의 위치
-        uint selectedMappingLength; //  소매상의 거래가 존재하는 mapping의 배열길이
-
+    function getDistributionChannelStackIndex(address retailTraderAddress, string itemName, uint mappingCounter)  public returns(bool, uint, uint){
+        uint selectedMappingLength = distributionChannelTable[mappingCounter].length;
         uint i;
-        
-        while(mappingCounter < maxMappingLength){
-            i=0;
-            selectedMappingLength = distributionChannelTable[mappingCounter].length;
-            while(i < selectedMappingLength){
-                if(retailTraderAddress == distributionChannelTable[mappingCounter][i]._buyerAddr){
-                    channelsInfo.push(getDistributionChannelStackInfo(
-                        true,
-                        mappingCounter,
-                        selectedMappingLength
-                    ));
-                }
-                i++;
+        while(i < selectedMappingLength){
+            if(retailTraderAddress == distributionChannelTable[mappingCounter][i]._buyerAddr){
+                return (true, mappingCounter, selectedMappingLength);
             }
-            mappingCounter++;
+            i++;
         }
-        if(channelsInfo.length != 0){
-            return (true, channelsInfo[0]._mappingCounter, channelsInfo[0]._selectedMappingLength);
-        }else{
-            return (false, 0, 0);
-        }
+        return (false, mappingCounter, selectedMappingLength);
     }
 
     // tx 1개 get
-	function getTx(uint arrayIndex, uint stackIndex) public constant returns(address, address, uint, string){
+	function getTx(uint arrayIndex, uint stackIndex) public constant returns(string, address, address, string, uint, uint, string){
 		currentTx storage a = distributionChannelTable[arrayIndex][stackIndex];
 		return (
-		  //  a._txIndex,
+		    a._txIndex,
 		    a._sellerAddr,
 		    a._buyerAddr,
-		  //  a._itemName,
-		  //  a._volum,
+		    a._itemName,
+		    a._volum,
 		    a._totalPrice,
 		    a._date
 		);
 	}
 
     //  이전거래인지 검증한다.
-    function pushTxOnPreviousTx(address sellerAddr, address buyerAddr, uint totalPrice, string date) returns (bool) {
+    function pushTxOnPreviousTx(string txIndex, address sellerAddr, address buyerAddr, string itemName, uint volum, uint totalPrice, string date) returns (bool) {
         uint arrayCounter;
         uint stackCounter;
         bool result = false;
@@ -112,11 +99,11 @@ contract DistributionChannelTrace {
             while(stackCounter < distributionChannelTable[arrayCounter].length){
                 if(sellerAddr == distributionChannelTable[arrayCounter][stackCounter]._buyerAddr){
                     distributionChannelTable[arrayCounter].push(currentTx(
-                        // txIndex,
+                        txIndex,
                         sellerAddr,
                         buyerAddr,
-                        // itemName,
-                        // volum,
+                        itemName,
+                        volum,
                         totalPrice,
                         date
                     ));
@@ -132,13 +119,13 @@ contract DistributionChannelTrace {
 
     //  이 코드위치는 유통 경로 테이블에 값이 있고 이전거래가 없는 상태이다.
     //  즉 테이블에 값이 있고 이전거래가 없어서 새로운 유통 경로를 생성하는 것이다.
-    function pushTxForNewDistributionChannel(address sellerAddr, address buyerAddr, uint totalPrice, string date){
+    function pushTxForNewDistributionChannel(string txIndex, address sellerAddr, address buyerAddr, string itemName, uint volum, uint totalPrice, string date){
         distributionChannelTable[getMappingLength()].push(currentTx(
-            // txIndex,
+            txIndex,
             sellerAddr,
             buyerAddr,
-            // itemName,
-            // volum,
+            itemName,
+            volum,
             totalPrice,
             date
         )); 
@@ -162,9 +149,3 @@ contract DistributionChannelTrace {
         }
 	}
 }
-
-
-
-
-
-
